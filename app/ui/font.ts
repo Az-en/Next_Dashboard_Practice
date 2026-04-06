f57c5b27b047d34a7520d41245957ecc6308d09f
Hafiz Azfar <azfarumer0764@gmail.com>
2026-04-06 22:35:16 +0500

    Added Styling, Added optimized custom font with next/font

---
+++ app/ui/font.ts
@@ -0,0 +1,6 @@
+import {Inter} from 'next/font/google';
+import {Lusitana} from 'next/font/google'
+
+export const inter = Inter({subsets:["latin"]})
+
+export const lusitana_normal = Lusitana({weight: "400"})
